775b08b19756ff4de33f07dbece1d7c16cf4e1f8
Tan Sakeer <tansakeer@gmail.com>
2023-08-07 00:31:33 +0000

    Added Comments model logic

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ApplicationRecord
   belongs_to :owner, class_name: "User"
+
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
+
+  has_many :comments, foreign_key: "owner_id"
+
 end
