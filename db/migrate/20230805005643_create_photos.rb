d8da2a1d16e1b27962728fc1c586f6f504d61bcc
Tan Sakeer <tansakeer@gmail.com>
2023-08-07 00:23:14 +0000

    generated photos

--- db/migrate/20230805005643_create_photos.rb
+++ db/migrate/20230805005643_create_photos.rb
@@ -2,10 +2,10 @@ class CreatePhotos < ActiveRecord::Migration[6.1]
   def change
     create_table :photos do |t|
       t.string :image
-      t.integer :comments_count
-      t.integer :likes_count
+      t.integer :comments_count, default: 0
+      t.integer :likes_count, default: 0
       t.text :caption
-      t.references :owner, null: false, foreign_key: true
+      t.references :owner, null: true, foreign_key: {to_table: :users} 
 
       t.timestamps
     end
