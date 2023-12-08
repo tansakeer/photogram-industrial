60fe1b7171d0ee15e1c6a587be9ffef0d43d55b1
Tan Sakeer <tansakeer@gmail.com>
2023-12-08 18:23:49 +0000

    added user hi

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   root "photos#index"
 
+  
   devise_for :users
 
   resources :comments
   resources :follow_requests
   resources :likes
   resources :photos
+
+  #get "user/:id" =>  "users/show", as: :user
+  resources :users, only: :show
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
